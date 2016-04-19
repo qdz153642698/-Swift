099608889d2005f20dce808e6f32c84414e2827e
qidazhi <qidazhi@beequick.cn>
2016-04-19 11:20:22 +0800

    Initial Commit

---
+++ 时光网Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  时光网Swift
+//
+//  Created by 亓大志 on 16/4/19.
+//  Copyright © 2016年 亓大志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
